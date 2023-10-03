a2ccc206243e13cde3491855c80a7bc4eab1f7d2
Pranav Thakral <pranavthakral22@gmail.com>
2023-10-03 17:26:13 +0530

    Initial layout for the to-do-management app

---
+++ src/main/java/com/example/pthakral/spring/ToDoManagementApp/ToDoManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.pthakral.spring.ToDoManagementApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoManagementAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pthakral/spring/ToDoManagementApp/ToDoManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pthakral.spring.ToDoManagementApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
