25f97fdd44ebbaa94bb5b2174c91735fcb34f1a3
Pranav Thakral <pranavthakral22@gmail.com>
2023-10-06 17:32:50 +0530

    Code for the login page

---
+++ src/main/java/com/example/pthakral/spring/ToDoManagementApp/login/LoginController.java
@@ -0,0 +1,18 @@
+package com.example.pthakral.spring.ToDoManagementApp.login;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class LoginController {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("login")
+    public String goToLoginPage() {
+        return "login";
+    }
+}
